bd0a3e5a38b390f82643437e5e9b6f8af12629cf
Bob Hauck <bobh@haucks.org>
2013-12-02 18:30:37 -0500

    Update to SDK 2.0 Beta 2.

--- src/simplday.c
+++ src/simplday.c
@@ -1,106 +1,60 @@
-#include "pebble_os.h"
-#include "pebble_app.h"
-#include "pebble_fonts.h"
+/****************************************************************************/
+/**
+* Pebble watch face that displays day, month, date, and time.
+*
+* @file   simplday.c
+*
+* @author Bob Hauck <bobh@haucks.org>
+*
+* This code may be used or modified in any way whatsoever without permission
+* from the author.
+*
+*****************************************************************************/
+#include "pebble.h"
 
 
-#define MY_UUID { 0xB1, 0x92, 0xC2, 0xCE, 0xB2, 0x1D, 0x47, 0x9E,       \
-                  0x9C, 0xE2, 0x09, 0x0A, 0xDC, 0x60, 0x61, 0xEF }
-PBL_APP_INFO(MY_UUID,
-             "SimplDay", "Bob Hauck <bobh@haucks.org>",
-             1, 0, /* App version */
-             RESOURCE_ID_IMAGE_MENU_ICON,
-             APP_INFO_WATCH_FACE);
-
-Window window;
-TextLayer text_day_layer;
-TextLayer text_date_layer;
-TextLayer text_time_layer;
-Layer line_layer;
-
-int last_wday = -1;     /* used to tell when the day changes */
+Window *window;
+TextLayer *text_day_layer;
+TextLayer *text_date_layer;
+TextLayer *text_time_layer;
+Layer *line_layer;
+GFont *font_date;
+GFont *font_time;
 
 
+/* Draw the dividing line.
+*/
 void line_layer_update_callback(Layer *l, GContext *ctx)
 {
     (void)l;
 
     graphics_context_set_stroke_color(ctx, GColorWhite);
-    graphics_draw_line(ctx, GPoint(8, 97), GPoint(131, 97));
-    graphics_draw_line(ctx, GPoint(8, 98), GPoint(131, 98));
+    graphics_draw_line(ctx, GPoint(8, 81), GPoint(131, 81));
+    graphics_draw_line(ctx, GPoint(8, 82), GPoint(131, 82));
 }
 
 
-void handle_init(AppContextRef ctx)
+/* Called by the OS once per minute to update date and time.
+*/
+void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed)
 {
-    ResHandle res_d;
-    ResHandle res_t;
-    GFont font_date;
-    GFont font_time;
-
-    (void) ctx;
-    window_init(&window, "SimplDay");
-    window_stack_push(&window, true /* Animated */);
-    window_set_background_color(&window, GColorBlack);
-
-    resource_init_current_app(&APP_RESOURCES);
-
-    res_d = resource_get_handle(RESOURCE_ID_FONT_ROBOTO_CONDENSED_21);
-    res_t = resource_get_handle(RESOURCE_ID_FONT_ROBOTO_BOLD_SUBSET_49);
-    font_date = fonts_load_custom_font(res_d);
-    font_time = fonts_load_custom_font(res_t);
-
-    text_layer_init(&text_day_layer, window.layer.frame);
-    text_layer_set_text_color(&text_day_layer, GColorWhite);
-    text_layer_set_background_color(&text_day_layer, GColorClear);
-    layer_set_frame(&text_day_layer.layer, GRect(8, 44, 144-8, 168-44));
-    text_layer_set_font(&text_day_layer, font_date);
-    layer_add_child(&window.layer, &text_day_layer.layer);
-
-    text_layer_init(&text_date_layer, window.layer.frame);
-    text_layer_set_text_color(&text_date_layer, GColorWhite);
-    text_layer_set_background_color(&text_date_layer, GColorClear);
-    layer_set_frame(&text_date_layer.layer, GRect(8, 68, 144-8, 168-68));
-    text_layer_set_font(&text_date_layer, font_date);
-    layer_add_child(&window.layer, &text_date_layer.layer);
-
-    text_layer_init(&text_time_layer, window.layer.frame);
-    text_layer_set_text_color(&text_time_layer, GColorWhite);
-    text_layer_set_background_color(&text_time_layer, GColorClear);
-    layer_set_frame(&text_time_layer.layer, GRect(7, 92, 144-7, 168-92));
-    text_layer_set_font(&text_time_layer, font_time);
-    layer_add_child(&window.layer, &text_time_layer.layer);
-
-    layer_init(&line_layer, window.layer.frame);
-    line_layer.update_proc = &line_layer_update_callback;
-    layer_add_child(&window.layer, &line_layer);
-
-    last_wday = -1;
-}
-
-
-void handle_minute_tick(AppContextRef ctx, PebbleTickEvent *t)
-{
-    (void)ctx;
-
-    // Need to be static because they're used by the system later.
+    /* Need to be static because they're used by the system later. */
     static char date_text[] = "Xxxxxxxxx 00";
     static char day_text[] = "Xxxxxxxxx";
     static char time_text[] = "00:00";
     char *time_format;
 
-    if (t->tick_time->tm_wday != last_wday)
+    if (units_changed & DAY_UNIT)
     {
-        string_format_time(day_text, sizeof(day_text), "%A", t->tick_time);
-        text_layer_set_text(&text_day_layer, day_text);
+        strftime(day_text, sizeof(day_text), "%A", tick_time);
+        text_layer_set_text(text_day_layer, day_text);
 
-        string_format_time(date_text, sizeof(date_text), "%B %e", t->tick_time);
-        text_layer_set_text(&text_date_layer, date_text);
-
-        last_wday = t->tick_time->tm_wday;
+        strftime(date_text, sizeof(date_text), "%B %e", tick_time);
+        text_layer_set_text(text_date_layer, date_text);
     }
 
     time_format = clock_is_24h_style() ? "%R" : "%I:%M";
-    string_format_time(time_text, sizeof(time_text), time_format, t->tick_time);
+    strftime(time_text, sizeof(time_text), time_format, tick_time);
 
     // Kludge to handle lack of non-padded hour format string
     // for twelve hour clock.
@@ -109,21 +63,82 @@ void handle_minute_tick(AppContextRef ctx, PebbleTickEvent *t)
         memmove(time_text, &time_text[1], sizeof(time_text) - 1);
     }
 
-    text_layer_set_text(&text_time_layer, time_text);
+    text_layer_set_text(text_time_layer, time_text);
 }
 
 
-void pbl_main(void *params)
+/* Initialize the application.
+*/
+void app_init(void)
 {
-    PebbleAppHandlers handlers =
-    {
-        .init_handler = &handle_init,
-        .tick_info =
-        {
-            .tick_handler = &handle_minute_tick,
-            .tick_units = MINUTE_UNIT
-        }
-    };
+    time_t t = time(NULL);
+    struct tm *tick_time = localtime(&t);
+    TimeUnits units_changed = SECOND_UNIT | MINUTE_UNIT | HOUR_UNIT | DAY_UNIT;
+    ResHandle res_d;
+    ResHandle res_t;
+    Layer *window_layer;
 
-    app_event_loop(params, &handlers);
+    window = window_create();
+    window_layer = window_get_root_layer(window);
+    window_set_background_color(window, GColorBlack);
+
+    res_d = resource_get_handle(RESOURCE_ID_FONT_ROBOTO_CONDENSED_21);
+    res_t = resource_get_handle(RESOURCE_ID_FONT_ROBOTO_BOLD_SUBSET_49);
+    font_date = fonts_load_custom_font(res_d);
+    font_time = fonts_load_custom_font(res_t);
+
+    text_day_layer = text_layer_create(GRect(8, 44, 144-8, 168-44));
+    text_layer_set_text_color(text_day_layer, GColorWhite);
+    text_layer_set_background_color(text_day_layer, GColorClear);
+    text_layer_set_font(text_day_layer, font_date);
+    layer_add_child(window_layer, text_layer_get_layer(text_day_layer));
+
+    text_date_layer = text_layer_create(GRect(8, 68, 144-8, 168-68));
+    text_layer_set_text_color(text_date_layer, GColorWhite);
+    text_layer_set_background_color(text_date_layer, GColorClear);
+    text_layer_set_font(text_date_layer, font_date);
+    layer_add_child(window_layer, text_layer_get_layer(text_date_layer));
+
+    text_time_layer = text_layer_create(GRect(7, 92, 144-7, 168-92));
+    text_layer_set_text_color(text_time_layer, GColorWhite);
+    text_layer_set_background_color(text_time_layer, GColorClear);
+    text_layer_set_font(text_time_layer, font_time);
+    layer_add_child(window_layer, text_layer_get_layer(text_time_layer));
+
+    line_layer = layer_create(layer_get_frame(window_layer));
+    layer_set_update_proc(line_layer, line_layer_update_callback);
+    layer_add_child(window_layer, line_layer);
+
+    handle_minute_tick(tick_time, units_changed);
+    window_stack_push(window, true /* Animated */);
+    tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
+}
+
+
+/* Shut down the application
+*/
+void app_term(void)
+{
+    tick_timer_service_unsubscribe();
+    text_layer_destroy(text_time_layer);
+    text_layer_destroy(text_date_layer);
+    text_layer_destroy(text_day_layer);
+    layer_destroy(line_layer);
+
+    window_destroy(window);
+
+    fonts_unload_custom_font(font_date);
+    fonts_unload_custom_font(font_time);
+}
+
+
+/********************* Main Program *******************/
+
+int main(void)
+{
+    app_init();
+    app_event_loop();
+    app_term();
+
+    return 0;
 }
